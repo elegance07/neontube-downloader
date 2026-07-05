861ac2f31534edfb4b98b90c69249d97f037533d
elegance07 <elegance07@users.noreply.github.com>
2026-07-05 12:37:02 +0300

    Remove local startup script from repository

--- baslat.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Neon-Tube Medya Konsolu
-cd /d "%~dp0"
-echo Gerekli kutuphaneler kontrol ediliyor (yt-dlp ve streamlit)...
-py -m pip install --upgrade yt-dlp streamlit
-echo.
-echo Neon-Tube Medya Konsolu Baslatiliyor...
-py -m streamlit run downloader.py
-pause
